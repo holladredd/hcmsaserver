2308b152b60d734be4193162dec62df90384d7a8
olamide folayan <holladred@gmail.com>
2024-12-25 11:10:12 +0100

    ayth

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
     username: {
       type: String,
       required: true,
@@ -27,12 +28,12 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
 module.exports = mongoose.model("User", userSchema);
