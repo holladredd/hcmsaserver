3b743418cbbf2050db676170f0e580a806ed0d90
olamide folayan <84618342+holladredd@users.noreply.github.com>
2025-01-24 12:49:07 +0100

    Add files via upload

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,16 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
 
-const protect = async (req, res, next) => {
-  let token;
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select("-password");
-      next();
-    } catch (error) {
-      res.status(401).json({ message: "Not authorized" });
-    }
-  }
+  if (!token) return res.status(401).json({ message: "Access token required" });
 
-  if (!token) {
-    res.status(401).json({ message: "Not authorized, no token" });
-  }
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.user = user;
+    next();
+  });
 };
 
-module.exports = { protect };
+module.exports = { authenticateToken };
