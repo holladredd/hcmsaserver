50ec7f46eb4fbd6431ffc0b4864ba55d8b02493e
olamide folayan <holladred@gmail.com>
2025-01-25 14:28:42 +0100

    Re-enable password hashing in User model and auth controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const {
   generateAccessToken,
   generateRefreshToken,
@@ -16,12 +16,12 @@ const authController = {
         return res.status(400).json({ message: "User already exists" });
       }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+      // const hashedPassword = await bcrypt.hash(password, 10);
 
       const user = await User.create({
         username,
         email,
-        password: hashedPassword,
+        password: password,
         role: role || "patient",
       });
 
